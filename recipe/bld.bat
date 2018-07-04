e5119de016666ea3ffc48d54b7967a5d7df9d6d1
Isuru Fernando <isuruf@gmail.com>
2018-07-03 18:41:09 -0600

    Build tests unconditionally and use CFLAGS and CXXFLAGS instead of CPPFLAGS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,10 @@
 mkdir build
 cd build
-if "%PY_VER%" == "2.7" (
-    set TEST=OFF
-) else (
-    set TEST=ON
-)
 
-set "CPPFLAGS=/DUNITY_EXCLUDE_STDINT_H /DUNITY_POINTER_WIDTH=64"
+set "CFLAGS=%CFLAGS% /DUNITY_EXCLUDE_STDINT_H /DUNITY_POINTER_WIDTH=64"
+set "CXXFLAGS=%CXXFLAGS% /DUNITY_EXCLUDE_STDINT_H /DUNITY_POINTER_WIDTH=64"
 
-cmake -G "NMake Makefiles" -D ENABLE_DRAFTS=OFF -D WITH_PERF_TOOL=OFF -D ZMQ_BUILD_TESTS=%TEST% -D ENABLE_CPACK=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D ENABLE_DRAFTS=OFF -D WITH_PERF_TOOL=OFF -D ZMQ_BUILD_TESTS=ON -D ENABLE_CPACK=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
@@ -19,9 +15,6 @@ copy /y %LIBRARY_BIN%\libzmq-mt-4*.dll /b %LIBRARY_BIN%\libzmq.dll
 if errorlevel 1 exit 1
 copy /y %LIBRARY_LIB%\libzmq-mt-4*.lib /b %LIBRARY_LIB%\libzmq.lib
 if errorlevel 1 exit 1
-
-REM tests can't be built on VC 9
-if "%PY_VER%" == "2.7" exit 0
 
 .\bin\test_ancillaries
 .\bin\test_atomics
